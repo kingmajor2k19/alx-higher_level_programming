e8c806731e7604a605715ba8ab9b44184474bdc6
kingmajor2k19 <stephentimothy657@gmail.com>
2022-12-02 21:30:24 -0800

    interger

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 
-def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = 0, 0
-        else:
-            tuple_a = tuple_a[0], 0
-
-    if len(tuple_b) < 2:
-        if len(tuple_b) == 0:
-            tuple_b = 0, 0
-        else:
-            tuple_b = tuple_b[0], 0
-    
-    added_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    return added_tuple
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for column in row:
+            if column == row[-1]:
+                print('{:d}'.format(column), end='')
+            else:
+                print('{:d}'.format(column), end=' ')
+        print()
