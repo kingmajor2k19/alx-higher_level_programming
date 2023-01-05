f0ad253c9f876ceca0181093182a0c1b53ff5160
kingmajor2k19 <stephentimothy657@gmail.com>
2023-01-05 12:27:47 -0800

    done

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,13 +9,13 @@ def matrix_divided(matrix, div):
     Args:
         matrix: A list of lists (matrix)- members can be of type ints or floats
         div: Number to be used for the division (can be a float or an integer)
+    Returns:
+            A new matrix which represents the result of the divisions
     Raises:
         TypeError: If the matrix contains non-numbers
         TypeError: If the matrix contains rows of different sizes
         TypeError: If div is not an int or float
         ZeroDivisionError: If div is 0
-    Returns:
-        A new matrix which represents the result of the divisions
     """
 
     if not type(div) in (int, float):
